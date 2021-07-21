69aa891acf92fbea0aace8d7017d9864b8bc0ab4
mandresblanco <mandresblanco@outlook.com>
2021-07-21 17:32:34 -0500

    commit

--- scraper.py
+++ scraper.py
@@ -1,12 +1,51 @@
 import requests
 import lxml.html as html
+import os
+import datetime
 
 HOME_URL = 'https://www.larepublica.co/'
 
 XPATH_LINK_TO_ARTICLE = '//a[@class = "empresasSect" or @class = "alta-gerenciaSect" or @class = "economiaSect" or @class = "finansasSect" or @class = "empresasSect" or @class = "actualidadSect" or @class = "globoeconomiaSect" or @class = "editorial-opinionSect" or @class = "ocioSect" or @defaultSect]/@href'
-XPATH_TITLE = '//div[@class = "mb-auto" or @class = "col order-2"]/h2/span/text()'
-XPATH_SUMMARY = '//div[@class= "lead"]/p/text()'
-XPATH_BODY = '//div[@class = "html-content"]/p//text()'
+XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
+XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
+XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
+
+
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                print(title)
+                title = title.replace('\"','')
+                title = title.replace('/','-')
+                title = title.replace('|','-')
+                title = title.replace(':','-')
+                title = title[0:80]
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+            
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n\n')
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
 
 def parse_home():
     try:
@@ -15,16 +54,94 @@ def parse_home():
             Home = response.content.decode('utf-8')
             parsed = html.fromstring(Home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            print(links_to_notices)
+            # print(links_to_notices)
+            today = datetime.date.today().strftime('%d-%m-%Y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+
+            for link in links_to_notices:
+                parse_notice(link, today)
+
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
         print(ve)
+
 def run():
     parse_home()
+
 
 if __name__ == '__main__':
     run()
 
+# import requests
+# import lxml.html as html
+# import os
+# import datetime
+
+# HOME_URL = 'https://www.larepublica.co/'
+
+# XPATH_LINK_TO_ARTICLE = '//text-fill/a[@class="economiaSect" or @class="empresasSect" or @class="ocioSect" or @class="globoeconomiaSect" or @class="analistas-opinionSect"]/@href'
+# XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
+# XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
+# XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
 
 
+# def parse_news(link, today):
+#     try:
+#         response = requests.get(link)
+#         if response.status_code == 200:
+#             notice = response.content.decode('utf-8')
+#             parsed = html.fromstring(notice)
+
+#             try:
+#                 title = parsed.xpath(XPATH_TITLE)[0]
+#                 print(title)
+#                 title = title.replace('\"','')
+#                 title = title.replace('/','-')
+#                 title = title.replace('|','-')
+#                 title = title.replace(':','-')
+#                 title = title[0:80]
+#                 summary = parsed.xpath(XPATH_SUMMARY)[0]
+#                 body = parsed.xpath(XPATH_BODY)
+#             except IndexError:
+#                 return
+
+#             with open(f'{today}/{title}.txt','w', encoding='utf-8') as f:
+#                 f.write(title)
+#                 f.write('\n\n')
+#                 f.write(summary)
+#                 f.write('\n\n')
+#                 for p in body:
+#                     f.write(p)
+#                     f.write('\n')
+#         else:
+#             raise ValueError(f'Error: {response.status_code}')
+#     except ValueError as ve:
+#         print(ve)
+
+
+# def parse_home():
+#     try:
+#         response = requests.get(HOME_URL)
+#         if response.status_code == 200:
+#             home = response.content.decode('utf-8')
+#             parsed = html.fromstring(home)
+#             links_to_news = parsed.xpath(XPATH_LINK_TO_ARTICLE)
+#             #print(links_to_news)
+#             today = datetime.date.today().strftime('%d-%m-%Y')
+#             if not os.path.isdir(today):
+#                 os.mkdir(today)
+#             for link in links_to_news:
+#                 parse_news(link, today)
+#         else:
+#             raise ValueError(f'Error: {response.status_code}')
+#     except ValueError as ve:
+#         print(ve)
+
+# def run():
+#     parse_home()
+
+
+# if __name__ == '__main__':
+#     run()
